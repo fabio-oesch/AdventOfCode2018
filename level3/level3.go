664ad41045beb0cc393855d7ea514bd6794a6908
Fabio L. Oesch <fabiooesch@hotmail.com>
2019-08-28 21:10:21 +0100

    2nd star level 3

--- level3/level3.go
+++ level3/level3.go
@@ -9,17 +9,47 @@ import (
 
 func main() {
 	filename := "testinput.txt"
-	squareSize := 1500
+	//squareSize := 10
+	squareSize := 1000
 	fabric := make([][]int, squareSize)
 	for row := range fabric {
 		fabric[row] = make([]int, squareSize)
 	}
 	inputs := readAndSplitFile(filename)
+	claimAmount := make([]bool, 1266)
+	for i, input := range inputs {
+		claimFabric2(fabric, input, i + 1, claimAmount)
+	}
+	for i := range claimAmount {
+		if !claimAmount[i] {
+			fmt.Println(i)
+		}
+	}
+}
+
+func firstStar(fabric [][]int, inputs []string) {
 	overlappingPieces := 0
 	for _, input := range inputs {
 		overlappingPieces += claimFabric(fabric, input)
 	}
 	fmt.Println(overlappingPieces)
+}
+
+func claimFabric2(fabric [][]int, claim string, currentClaimNumber int, claimAmount []bool) {
+	indexOfAt := strings.Index(claim, "@")
+	indexOfColon := strings.Index(claim, ":")
+	x, y := getXandYPos(claim[indexOfAt+2 : indexOfColon])
+	i, j := getDimensions(claim[indexOfColon+2:])
+	for row := x; row < x+i; row += 1 {
+		for col := y; col < y+j; col += 1 {
+			if fabric[row][col] != 0 {
+				claimAmount[currentClaimNumber] = true
+				claimAmount[fabric[row][col]] = true
+			} else {
+				fabric[row][col] = currentClaimNumber
+			}
+		}
+	}
 }
 
 func claimFabric(fabric [][]int, claim string) int {
@@ -28,8 +58,8 @@ func claimFabric(fabric [][]int, claim string) int {
 	indexOfColon := strings.Index(claim, ":")
 	x, y := getXandYPos(claim[indexOfAt+2 : indexOfColon])
 	i, j := getDimensions(claim[indexOfColon+2:])
-	for row := x; row < x + i; row += 1 {
-		for col := y; col < y + j; col += 1 {
+	for row := x; row < x+i; row += 1 {
+		for col := y; col < y+j; col += 1 {
 			fabric[row][col] += 1
 			if fabric[row][col] == 2 {
 				overlapping += 1
