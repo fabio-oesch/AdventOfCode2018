834c211f3f990d615c21cf3014afab49cbf35b07
Fabio L. Oesch <fabiooesch@hotmail.com>
2019-08-28 18:59:39 +0100

    First Gold Star

--- level1/level1.go
+++ level1/level1.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	filename := "input.txt"
-	input := readAndSplitFile(filename)
-	fmt.Println(input)
+	filename := "testinput.txt"
+	inputs := readAndSplitFile(filename)
+	fmt.Println(sumUpValues(inputs))
+}
+
+func sumUpValues(inputs []string) int {
+	result := 0
+	for _, input := range inputs {
+		if len(input) == 0 {
+			break
+		}
+		toInt, err := strconv.Atoi(input[1:])
+		if err != nil {
+			panic(err)
+		}
+		if input[0] == 43 {
+			result += toInt
+		} else {
+			result -= toInt
+		}
+	}
+	return result
 }
 
 func readAndSplitFile(filename string) []string {
-	input, err := ioutil.ReadFile("input.txt")
+	input, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
