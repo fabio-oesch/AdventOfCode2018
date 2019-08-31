215a67eeb0814bf21d430fa0fdc85cbb61f364de
Fabio L. Oesch <fabiooesch@hotmail.com>
2019-08-31 11:28:15 +0100

    changed the method from map to slice
    The map didn't work as intended so I changed it to a slice. Now the only
    thing in the instructions are the pointers. I also use a way to create
    methods on the objects as well as created a `String()` method because it
    was difficult to understand if my code did what it was intended to do.

--- level7/level7.go
+++ level7/level7.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 )
 
+type instructions []*instruction
+
 type instruction struct {
 	name     string
 	follows  *instruction
@@ -15,29 +17,73 @@ type instruction struct {
 func main() {
 	filename := "testinput.txt"
 	inputs := readAndSplitFile(filename)
-	instructions := make(map[string]instruction)
-	createHierarchy(inputs, instructions)
-	fmt.Println(instructions["A"].follows.precedes)
-	fmt.Println(instructions)
-}
-
-func createHierarchy(inputs []string, instructions map[string]instruction) {
+	inst := instructions{}
 	for _, input := range inputs {
 		split := strings.Fields(input)
 		parent := split[1]
 		child := split[7]
-		childInstruction, ok := instructions[child]
-		if !ok {
-			 childInstruction = instruction{child, nil, nil}
+		var parentInst *instruction
+		var childInst *instruction
+		if inst.exists(parent) {
+			parentInst = inst.find(parent)
+		} else {
+			parentInst = inst.add(parent)
 		}
-		parentInstruction, ok := instructions[parent]
-		if !ok {
-			parentInstruction = instruction{parent, nil, []*instruction{}}
+		if inst.exists(child) {
+			childInst = inst.find(child)
+		} else {
+			childInst = inst.add(child)
 		}
-		childInstruction.follows = &parentInstruction
-		parentInstruction.precedes = append(parentInstruction.precedes, &childInstruction)
-		instructions[parent], instructions[child] = parentInstruction, childInstruction
+		childInst.follows = parentInst
+		parentInst.precedes = append(parentInst.precedes, childInst)
+		fmt.Println(inst)
 	}
+	//instructions := make(map[string]instruction)
+	//createHierarchy(inputs, instructions)
+	fmt.Println(inst[0].name)
+}
+
+func (inst instructions) find(name string) *instruction {
+	for _, i := range inst {
+		if (*i).name == name {
+			return i
+		}
+	}
+	return nil
+}
+
+func (inst instructions) exists(name string) bool {
+	for _, i := range inst {
+		if (*i).name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func (inst *instructions) add(name string) *instruction {
+	i := instruction{name, nil, nil}
+	*inst = append(*inst, &i)
+	return &i
+}
+
+func (inst instructions) String() string {
+	var out strings.Builder
+	for _, i := range inst {
+		fmt.Fprintf(&out, "name: "+i.name)
+		if i.follows != nil {
+			fmt.Fprintf(&out, ", follows: "+i.follows.name)
+		}
+		if len(i.precedes) > 0 {
+			fmt.Fprintf(&out, ", precedes: ["+i.precedes[0].name)
+			for _, p := range i.precedes[1:] {
+				fmt.Fprintf(&out, " "+p.name)
+			}
+			fmt.Fprintf(&out, "]")
+		}
+		fmt.Fprintf(&out, "; ")
+	}
+	return out.String()
 }
 
 func readAndSplitFile(filename string) []string {
